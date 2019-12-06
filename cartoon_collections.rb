d838243cab593eff4fad7c6232ebdda20452a811
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-12-06 02:24:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map { |ele| ele[0].upca
+  array.map { |ele| ele[0].upcase + ele[1..-1].downcase + "!" }
 end
 
 def long_planeteer_calls# code an argument here
